37fab47e1859a2ed332a8c498f1b95c609694733
Luis Nuñez <luisnunez91@gmail.com>
2026-06-28 16:10:41 -0600

    small components

--- my-dashboard/app/dashboard/counter/page.tsx
+++ my-dashboard/app/dashboard/counter/page.tsx
@@ -1,26 +1,22 @@
-'use client'
-import { useState } from "react";
+
+import { CartCounter } from "@/app/shopping-cart";
 
 export const metadata = {
-    title: 'Counter Page',
+    title: 'Shooping Cart',
     description: 'Counter Page',
 }
 
 export default function CounterPage() {
 
 
-    const [count, setCount] = useState(0);
+
 
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <span>Productos en el carrito: </span>
-            <span className="text-9xl">{count}</span>
+            <CartCounter value={20} />
 
-            <div className="flex">
-                <button onClick={() => setCount(count + 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">+1</button>
-                <button onClick={() => setCount(count - 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">-1</button>
 
-            </div>
         </div>
     )
 }

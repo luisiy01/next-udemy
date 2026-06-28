098366c418799955558e78d8e87dfde6fe2fa608
Luis Nuñez <luisnunez91@gmail.com>
2026-06-28 15:00:30 -0600

    manejo de estados

--- my-dashboard/app/dashboard/counter/page.tsx
+++ my-dashboard/app/dashboard/counter/page.tsx
@@ -1,6 +1,26 @@
+'use client'
+import { useState } from "react";
+
+export const metadata = {
+    title: 'Counter Page',
+    description: 'Counter Page',
+}
 
 export default function CounterPage() {
+
+
+    const [count, setCount] = useState(0);
+
     return (
-        <div>CounterPage</div>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos en el carrito: </span>
+            <span className="text-9xl">{count}</span>
+
+            <div className="flex">
+                <button onClick={() => setCount(count + 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">+1</button>
+                <button onClick={() => setCount(count - 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">-1</button>
+
+            </div>
+        </div>
     )
 }

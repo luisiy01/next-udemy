6a68044118ef638b2018f2126fbdeb63fed891b7
Luis Nuñez <luisnunez91@gmail.com>
2026-07-02 09:43:17 -0600

    error page

--- my-dashboard/app/dashboard/pokemons/page.tsx
+++ my-dashboard/app/dashboard/pokemons/page.tsx
@@ -8,6 +8,10 @@ const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => 
         id: Number(pokemon.url.split('/').at(-2)!),
         name: pokemon.name
     }))
+
+
+    //throw new Error('Esto es un error')
+
     return pokemons;
 }
 

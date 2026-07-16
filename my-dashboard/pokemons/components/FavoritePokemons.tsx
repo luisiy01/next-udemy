de5e73626074c932056086d0ee89e63e5437af10
Luis Nuñez <luisnunez91@gmail.com>
2026-07-16 16:38:29 -0600

    mantener favoritos

--- my-dashboard/pokemons/components/FavoritePokemons.tsx
+++ my-dashboard/pokemons/components/FavoritePokemons.tsx
@@ -1,11 +1,23 @@
 'use client'
 import { useAppSelector } from '@/store';
 import { PokemonGrid } from './PokemonGrid';
+import { IoHeartOutline } from 'react-icons/io5';
 
 export const FavoritePokemons = () => {
     const favoritePokemons = useAppSelector(state => Object.values(state.pokemons));
 
     return (
-        <PokemonGrid pokemons={favoritePokemons} />
+        favoritePokemons.length === 0
+            ? <NoFavorites />
+            : <PokemonGrid pokemons={favoritePokemons} />
+    )
+}
+
+export const NoFavorites = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-[50vh]">
+            <IoHeartOutline size={100} className="text-red-500" />
+            <h1 className="text-xl font-semibold">No hay favoritos</h1>
+        </div>
     )
 }
